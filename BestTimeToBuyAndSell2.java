5b6520f53bdfbcfb2930ef4aa8bc96535f9a65b4
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2020-09-19 15:19:50 +0530

    Add files via upload

---
+++ BestTimeToBuyAndSell2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max=0;
+        if(prices.length<=1)
+            return 0;
+        
+        for(int i=0;i<prices.length-1;i++)
+        {
+            if(prices[i]<prices[i+1])
+            {
+                max=max+(prices[i+1]-prices[i]);
+            }
+        }
+        return max;
+    }
+}
